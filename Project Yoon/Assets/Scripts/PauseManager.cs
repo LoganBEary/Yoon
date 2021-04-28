55fca83cbbca1e59ea6e1b167f47ddfe1c3c881a
jfawcet5 <joshfawcettshs10@gmail.com>
2021-04-27 19:06:59 -0700

    Added pause menu

---
+++ Project Yoon/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (paused)
+        {
+            Time.timeScale = 0;
+            pauseMenuUI.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenuUI.SetActive(false);
+        }
+    }
+}
