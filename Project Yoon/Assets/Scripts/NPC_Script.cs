356baba8866f39d07502dee644f3be08152276cd
HansMPrieto <hansmiguelpprieto@gmail.com>
2021-04-29 23:35:11 -0700

    Changed NPC interaction to be within a certain distance

--- Project Yoon/Assets/Scripts/NPC_Script.cs
+++ Project Yoon/Assets/Scripts/NPC_Script.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
+
 
 public class NPC_Script : MonoBehaviour
 {
     // Reference to the child of the NPC gameobject that displays text
+    float distance;
     public GameObject displayTextCanvas;
+    public GameObject player;
+    public float distance_;
+    private float is_interact = 0;
+
 
     // This function will be called whenever the player presses the "E" key
-    public void OnPlayerInteract()
+    public void OnPlayerInteract(InputValue val)
     {
         // Call the displayText coroutine to display the text for 5 seconds
-        StartCoroutine(displayText());
+        is_interact = val.Get<float>();
     }
 
     // Coroutine to display the text for 5 seconds
     private IEnumerator displayText()
     {
-
         // Set displayTextCanvas to active
         displayTextCanvas.SetActive(true);
         yield return new WaitForSeconds(5f);
@@ -25,5 +31,15 @@ public class NPC_Script : MonoBehaviour
         // Set displayTextCanvas to inactive
         displayTextCanvas.SetActive(false);
         yield return new WaitForSeconds(5f);
+    }
+
+    void FixedUpdate()
+    {
+        distance_ = Vector3.Distance(player.transform.position, transform.position);
+        if (distance_ <= 1.5 && is_interact == 1) 
+        {
+            StartCoroutine(displayText());
+        }
+        is_interact = 0;
     }
 }
