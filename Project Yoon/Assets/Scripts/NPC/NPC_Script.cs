3cc3c60ecf810b5a1a7df9fe3b076c8c474bd155
HansMPrieto <hansmiguelpprieto@gmail.com>
2021-05-06 18:49:37 -0700

    Added transition between texts for NPC

--- Project Yoon/Assets/Scripts/NPC/NPC_Script.cs
+++ Project Yoon/Assets/Scripts/NPC/NPC_Script.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro; 
 
 public class NPC_Script : MonoBehaviour
 {
     // Reference to the child of the NPC gameobject that displays text
     float distance;
     public GameObject displayTextCanvas;
+    public Text dialogueText; 
     public GameObject prompt;
     public GameObject player;
     public float distance_;
+    public string[] dialogueLines;
     private float is_interact = 0;
-
-
+    
     // This function will be called whenever the player presses the "E" key
     public void OnPlayerInteract(InputValue val)
     {
@@ -22,16 +24,23 @@ public class NPC_Script : MonoBehaviour
         is_interact = val.Get<float>();
     }
 
+    
     // Coroutine to display the text for 5 seconds
     private IEnumerator displayText()
     {
         // Set displayTextCanvas to active
         displayTextCanvas.SetActive(true);
-        yield return new WaitForSeconds(5f);
+        
+        for(int i = 0; i < dialogueLines.Length; i++)
+        {
+            dialogueText.text = dialogueLines[i];
+            dialogueText.enabled = true;
+            yield return new WaitForSeconds(3f);
+            dialogueText.text = "";
+        }
 
         // Set displayTextCanvas to inactive
         displayTextCanvas.SetActive(false);
-        yield return new WaitForSeconds(5f);
     }
 
     void FixedUpdate()
