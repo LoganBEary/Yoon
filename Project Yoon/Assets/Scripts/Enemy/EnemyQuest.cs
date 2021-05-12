aa1043d8500d25151e03d9d9f25f0d793bc09995
LoganBEary <loganlevitre@gmail.com>
2021-05-11 22:04:44 -0700

    quest text

--- Project Yoon/Assets/Scripts/Enemy/EnemyQuest.cs
+++ Project Yoon/Assets/Scripts/Enemy/EnemyQuest.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyQuest : MonoBehaviour
 {
     public string questName;
+    public TextMeshProUGUI questText;
 
     public void onDeath()
     {
         GameManager.gameManager.notifyEvent(questName);
+        questText.enabled = false;
         return;
     }
 }
---
+++ Project Yoon/Assets/Scripts/NPC/QuestsScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class QuestsScript : MonoBehaviour
+{
+    public bool isActive;
+    public string title;
+    public string description;
+    public int experienceGained;
+    public int reward;
+}
